200cd07324fd6fb07b798955063838bd4c450232
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-29 20:43:45 -0600

    Update Punto 5.py

--- Punto 5.py
+++ Punto 5.py
@@ -2,31 +2,32 @@ print("\nRuvalcaba Valverde Miguel Angel_1212_3W")
 print("---------------Examen 3-Punto 5---------------")
 class agenda():
     def __init__(self):
-        self.con = [0]#solo nesitaremos esta lista, al prinsipio pense en utilisar un dicionario pero es mas fasil añadir elementos a listas
+        self.con = []#solo nesitaremos esta lista, al prinsipio pense en utilisar un dicionario pero es mas fasil añadir elementos a listas
+        #Extra nota: al prinsipio la abia dejado como 0 pero me acorde que se podia dejar vacia la lista
     def ac(self):#Primero haemos esta funcion
         name = input("Ingrese el nombre: ")#capturamos y creamos datos
         te = input("Ingrese telefono: ")
         co = input("Ingrese el correo electronico: ")
-        self.con.append(f"-♠-Contacto-♠-\nNombre: {name}\nTelefono: {te}\nCorreo: {co}\n")#imprimimos el mensaje y lo agregamos a la lista
+        self.con.append({"nombre": name, "telefono": te, "correo": co})#imprimimos el mensaje y lo agregamos a la lista
     def lis(self):
-            print("-•-Lista de contactos-•-")#aqui nomas mostramos la lista y fin
-            print(self.con)
-            
+        print("-•-Lista de contactos-•-") #estetica señores
+        for c in self.con: #ahora imprimimos todos los elementos de la lista, con el sigiente patron de abajo, no es el mejor pero es efectivo.
+            print(f"-♠-Contacto-♠-\nNombre: {c['nombre']}\nTelefono: {c['telefono']}\nCorreo: {c['correo']}\n")
     def busc(self):
         n = input("Buscar: ")#capturar dato
         for c in self.con:#iniciar bulce
-            if c["nombre"] == n():#aqui buscamos el
-                print(c)#y mostrarlo
+            if c["nombre"].lower() == n.lower():#aqui buscamos el contacto
+                print(f"-♠-Contacto-♠-\nNombre: {c['nombre']}\nTelefono: {c['telefono']}\nCorreo: {c['correo']}\n")#y aqui lo mostrarlo
                 return#a esto es pára que no se repita
             else:
-                print("No hay se encotro dicho contacto.")#esto es si no existe
+                print("no esta dicho contacto.")#mensaje de unexistecia
     def edit(self):
         n = input("Contacto que se editar: ")
         for c in self.con:#hasemos el mismo proses, pero capturando nuevos valores
-            if c["nombre"] == n():
-                newname = input("Ingrese el nuevo nombre (dejar vacío para no cambiar): ")
-                newtel = input("Ingrese el nuevo teléfono (dejar vacío para no cambiar): ")
-                newco = input("Ingrese el nuevo email (dejar vacío para no cambiar): ")
+            if c["nombre"].lower() == n.lower():
+                newname = input("Ingrese el nuevo nombre: ")
+                newtel = input("Ingrese el nuevo telefono: ")
+                newco = input("Ingrese el nuevo correo: ")
                 if newname:#ahora este if y elses alladimos los nuevos datos
                     c["nombre"] = newname
                 elif newtel:
@@ -35,12 +36,12 @@ class agenda():
                     c["corre"] = newco
                 print(f"Contacto {n} editado.")
                 return# lo mismo que el otro return
-            else:#esto es por si no tienes contactos y esta solito, Im sorry fo you
-                print("No hay se encotro dicho contacto.")
+        else:#esto es por si no tienes contactos y esta solito, Im sorry fo you
+            print("No hay se encotro dicho contacto.")
     def menu(self):
         et = 1
         while et > 0:#iniciamos otro bucle, al igual que undead unluck
-            print("\n--- Menú de Agenda ---")
+            print("\n--- Menu de Agenda ---")
             print("1. Añadir contacto")
             print("2. Lista de contactos")
             print("3. Buscar contacto")
